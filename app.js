ffbce603cda0db5bc7aee5424f8345b675649ad5
KamilMarcinowski <kamilmarcinowski2006@gmail.com>
2023-08-24 22:46:53 +0200

    commit2

--- app.js
+++ app.js
@@ -2,17 +2,39 @@ const textToAdd = document.getElementById("toAdd");
 const btnAdd = document.getElementById("Add");
 const contentDiv = document.getElementById("content");
 
+LoadData();
 
 function Add()
 {
-    const template = document.createElement("div");
-    template.style.width = "400px";
-    template.style.height = "50px";
-    template.style.backgroundColor = "white";
-    template.style.borderRadius = "15px";
-    template.style.marginTop = "10px";
+    if (textToAdd.value != "")
+    {
+        const template = document.createElement("div");
+        template.style.width = "400px";
+        template.style.height = "50px";
+        template.style.backgroundColor = "white";
+        template.style.borderRadius = "15px";
+        template.style.marginTop = "10px";
+        template.style.cursor = "pointer";
 
-    contentDiv.appendChild(template);
+        const title = document.createElement("p")
+        title.innerText = textToAdd.value;
+
+        template.appendChild(title);
+        contentDiv.appendChild(template);
+
+        textToAdd.value = "";
+        SaveData();
+    }
+}
+
+function SaveData()
+{
+    localStorage.setItem("data", contentDiv.innerHTML);
+}
+
+function LoadData()
+{
+    contentDiv.innerHTML = localStorage.getItem("data");
 }
 
 btnAdd.addEventListener("click", Add);
