3437af511bc3bd2c4ac0fb7c92cf5fe87d07439b
KamilMarcinowski <kamilmarcinowski2006@gmail.com>
2023-08-25 12:11:55 +0200

    commit3

--- app.js
+++ app.js
@@ -15,16 +15,48 @@ function Add()
         template.style.borderRadius = "15px";
         template.style.marginTop = "10px";
         template.style.cursor = "pointer";
+        template.className = "element";
 
         const title = document.createElement("p")
         title.innerText = textToAdd.value;
 
+        const deleteBtn = document.createElement("Button");
+        deleteBtn.innerText = "X";
+        deleteBtn.style.width = "30px";
+        deleteBtn.style.height = "30px";
+        deleteBtn.style.cursor = "pointer";
+        deleteBtn.style.float = "right";
+        deleteBtn.style.position = "relative";
+        deleteBtn.style.bottom = "30px";
+        deleteBtn.style.left = "-15px";
+        deleteBtn.style.backgroundColor = "red";
+        deleteBtn.style.border = "none";
+
+        deleteBtn.addEventListener("click", DeleteElement);
+
         template.appendChild(title);
+        template.appendChild(deleteBtn);
         contentDiv.appendChild(template);
 
         textToAdd.value = "";
         SaveData();
     }
+}
+
+function DeleteElement(e)
+{
+    window.onclick = e =>
+    {
+        if (e.target.parentElement.className == "element")
+        {
+            e.target.parentElement.remove();
+        }
+    }
+}
+
+function ClearList()
+{
+    contentDiv.innerHTML = "";
 }
 
 function SaveData()
